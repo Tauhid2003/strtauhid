4be3c2ccdd4854cb6ac0d2683d36501a49a3a822
SHAIK TAUHIDUR RAHMAN <24-58467-2@student.aiub.edu>
2025-10-12 03:11:47 +0600

    Initial portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+// small JS: insert current year and smooth scroll for internal links
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    const id = a.getAttribute('href');
+    if(id.length>1){
+      e.preventDefault();
+      document.querySelector(id).scrollIntoView({behavior:'smooth'});
+    }
+  })
+})
